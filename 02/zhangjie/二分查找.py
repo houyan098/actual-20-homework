64899a7a55f3766979f57729936cab270f40658d
martin <1335120568@qq.com>
2018-12-27 13:45:53 +0800

    20181223

---
+++ 02/zhangjie/二分查找.py
@@ -0,0 +1,15 @@
+def search(data_set,value):
+	low = 0
+	high = len(data_set) - 1
+
+	while low <= high:
+		mid = (low+high)//2
+		if data_set[mid] == value:
+			return mid
+		elif data_set[mid] > value:
+			high = mid -1
+		else:
+			low = mid + 1
+
+data_set =  list(range(100))
+print(search(data_set,36))
---
+++ 02/zhangjie/插入排序.py
@@ -0,0 +1,17 @@
+def insert_sort(ary):
+    n = len(ary)
+    for i in range(1,n):
+        if ary[i] < ary[i-1]:
+            temp = ary[i]
+            index = i           #待插入的下标
+            for j in range(i-1,-1,-1):  #从i-1 循环到 0 (包括0)
+                if ary[j] > temp :
+                    ary[j+1] = ary[j]
+                    index = j   #记录待插入下标
+                else :
+                    break
+            ary[index] = temp
+    return ary
+ary=[5,4,5,7,9,3,2,3,4]
+
+print(insert_sort(ary))
