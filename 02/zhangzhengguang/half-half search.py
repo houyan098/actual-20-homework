f7eddc1f5c435a94030ada08e8fd5d44f861dd01
Zhangzhengguang1 <45896915+Zhangzhengguang1@users.noreply.github.com>
2018-12-29 16:54:49 +0800

    Update half-half search.py

--- 02/zhangzhengguang/half-half search.py
+++ 02/zhangzhengguang/half-half search.py
@@ -7,16 +7,16 @@ x = 1
 guess = 2
 mol = 1
 '''
-使用列表系数的分子mol分母deno与计算次数x相关联的方式，第一次mol取2x，deno采用2**x，
-当大于guess下次计算原分子*2-1，小于时原分子/2+1
-未采用首尾相加折半取值的简洁算法，建议使用首尾相加模式
+使用列表长度系数系数分子mol分母deno与计算次数x相关联的方式，第一次mol取1(twice取2*mol+1or-1)，分母deno一直采用2**x，
+当大于guess时下一次分子取2*mol-1，小于guess时下一次分子取2*mol+1
+未采用首尾相加折半取值的简洁算法，建议使用首尾相加折中模式简单、好写、不易出错！！！
 '''
 while True:
     deno = 2**(x)
     '''这里分母deno直接取整会导致分母为0，后面出各种bug，
-    这里应该直接分子分母计算完后再取整'''
+    应直接分子分母计算完后再取整'''
     if list[int((mol /deno)*len(list))] == guess:
-        print("ok,find it:%s,used %s times"%(guess,x))
+        print("Ok,Find it: %s ,used %s times"%(guess,x))
         break
     elif list[int((mol /deno)*len(list))] > guess:
         mol = 2 * mol - 1
